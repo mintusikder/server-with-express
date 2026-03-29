1822a48e9e753f94b0af0435e804132ef40453dc
Mintu Sikder <mintusikder15@gmail.com>
2026-03-29 10:00:58 +0600

    added type express

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,16 +1,25 @@
 import { NextFunction, Response, Request } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 const auth = () => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
-    // console.log({ authToken: token });
-    if (!token) {
-      return res.status(500).json({ message: "Yow are na allow" });
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const authToken = req.headers.authorization;
+      const token = authToken?.trim().split(" ")[1]
+      console.log({ authToken: token });
+      if (!token) {
+        return res.status(500).json({ message: "Yow are na allow" });
+      }
+      const decoded = jwt.verify(token, config.jwtSecret as string);
+      console.log({ decoded });
+      req.user = decoded as JwtPayload;
+      next();
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: "Unauthorized: Invalid or expired token",
+      });
     }
-    const decoded = jwt.verify(token, config.jwtSecret as string);
-    console.log({ decoded });
-    next();
   };
 };
 
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -6,7 +6,7 @@ import auth from "../../middleware/auth";
 const router = express.Router();
 
 router.post("/", logger, userController.createUser);
-router.get("/", logger, auth, userController.getUsers);
+router.get("/", auth(), logger, userController.getUsers);
 router.get("/:id", logger, userController.getSingleUser);
 router.put("/:id", logger, userController.updateUser);
 router.delete("/:id", logger, userController.deleteUser);
